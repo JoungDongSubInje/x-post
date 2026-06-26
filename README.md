89f652e44d48efd4ef8c8dbf7416618dd658943e
JoungDongSubInje <pcdongsub@gmail.com>
2026-06-26 11:21:42 +0900

    Update) 0.1.3

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 이 컴포넌트는 코리아 오브의 X포스팅 코어 관련 컴포넌트압니다.
 
 ```rust
+use x-post::KoreaXPosting;
 
 fn main(){
     let v_korea_x_posts= KoreaXPosting::push_x_posing("url");
--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,29 @@
+#[derive(Debug)]
+pub struct Url{
+    base: String,
+    dns: String,
+    another_url: String
+}
+impl Url{
+    pub fn new(str_url: &'static str) -> Url{
+        let base= "https://".to_string(); // 
+        let dns= "x.com".to_string();
+        let another_url= str_url[13..].to_string();
+
+        Url { base, dns, another_url }
+    }
+}
+
+
 //  x 포스팅 데이터 스트럭쳐 -> url로 데이터 형성
 struct XPosting {
-    url: String,
+    url: Url,
 }
 
 // 에러 핸들링
 impl XPosting {
-    fn new(url: String) -> Option<Self> {
+    fn new(str_url: &'static str) -> Option<Self> {
+        let url= Url::new(str_url);
         Some(XPosting { url })
     }
 }
@@ -15,11 +33,11 @@ struct XPostingList {
     postings: Vec<XPosting>,
 }
 impl XPostingList {
-    pub fn new() -> Self{
+    fn new() -> Self{
         let postings: Vec<XPosting>= Vec::new();
         return XPostingList { postings };
     }
-    pub fn add_x_post(&mut self, x_post: XPosting) {
+    fn add_x_post(&mut self, x_post: XPosting) {
         self.postings.push(x_post);
     }
 }
@@ -28,12 +46,14 @@ pub struct KoreaXPosting;
 
 impl KoreaXPosting{
     // X 포스팅 url을 매개변수로 받기
-    pub fn push_x_posing(url: &'static str) -> XPostingList{
-        let url= url.to_string();
-
+    pub fn push_x_posing(post_query: &'static str) -> XPostingList{
         // 해당 x포스팅들을 익스포트 해서 리스트 만들어 데이터 넣기 
         let mut v_x_posts= XPostingList::new();
-        let option_x_post= XPosting::new(url);
+        
+        // static base) https://
+        // static base) x.com
+        // needed) /SpaceX/status/2065415377165726146
+        let option_x_post= XPosting::new(post_query);
         
         let x_post= match option_x_post {
             Some(XPosting) => XPosting,
